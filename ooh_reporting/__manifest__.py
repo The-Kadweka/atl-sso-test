ec1c0707c278db35f9641c601d6ff12399a6efd7
The-Kadweka <husseinkatana17@gmail.com>
2023-08-16 06:13:58 +0300

    uodate

--- ooh_reporting/__manifest__.py
+++ ooh_reporting/__manifest__.py
@@ -15,11 +15,12 @@
     'depends': ['purchase', 'account', 'sale_management','stock'],
     # always loaded
     'data': [
+        'reports/proformerInvoice.xml',
         'reports/purchaseOrder.xml',
         'reports/saleOrder.xml',
         'reports/view_inherit.xml',
         'reports/deliveryNote.xml',
-        'reports/withoutEtrInvoice.xml',
+        'reports/etrInvoice.xml',
         'views/printAction.xml'
     ],
 }
